2c4e5d8ed2b41b9e7fe50422c242c67802293e55
Bhaumik Muliya <bhaumikmuliya1304+github@gmail.com>
2022-08-25 17:53:53 +0530

    products added

--- src/constants/productlist.js
+++ src/constants/productlist.js
@@ -1,7 +1,51 @@
 const Products = [
   {
+    image: 'AQUASOOTHE PLUS.jpg',
+    text: 'AQUASOOTHE PLUS'
+  },
+  {
+    image: 'BEPOPREC.jpg',
+    text: 'BEPOPREC'
+  },
+  {
+    image: 'FLUOROPREC.jpg',
+    text: 'FLUOROPREC'
+  },
+  {
+    image: 'FLUOROPREC-T.jpg',
+    text: 'FLUOROPREC-T'
+  },
+  {
+    image: 'MOXIPREC-KT.jpg',
+    text: 'MOXIPREC-KT'
+  },
+  {
+    image: 'PARAPREC-1.jpg',
+    text: 'PARAPREC'
+  },
+  {
+    image: 'SCIBROM.jpg',
+    text: 'SCIBROM'
+  },
+  {
+    image: 'SCICLOP.jpg',
+    text: 'SCICLOP'
+  },
+  {
+    image: 'SCIDORZ-T.jpg',
+    text: 'SCIDORZ-T'
+  },
+  {
+    image: 'SCIFRESH GEL.jpg',
+    text: 'SCIFRESH GEL'
+  },
+  {
     image: 'SCIFRESH.jpg',
-    text: 'SciFresh'
+    text: 'SCIFRESH'
+  },
+  {
+    image: 'SCIGAT.jpg',
+    text: 'SCIGAT'
   },
   {
     image: 'SCI-HY.jpg',
@@ -16,6 +60,10 @@ const Products = [
     text: 'SCIMOX'
   },
   {
+    image: 'SCIMOX-BR.jpg',
+    text: 'SCIMOX-BR'
+  },
+  {
     image: 'SCIMOX-DX.jpg',
     text: 'SCIMOX-DX'
   },
@@ -24,8 +72,28 @@ const Products = [
     text: 'SCIMOX-LP'
   },
   {
+    image: 'SCIMOX-TB.jpg',
+    text: 'SCIMOX-TB'
+  },
+  {
+    image: 'SCINATA.jpg',
+    text: 'SCINATA'
+  },
+  {
     image: 'SCINEP.jpg',
     text: 'SCINEP'
+  },
+  {
+    image: 'SCIPRED.jpg',
+    text: 'SCIPRED'
+  },
+  {
+    image: 'TOBAPREC-LP.jpg',
+    text: 'TOBAPREC-LP'
+  },
+  {
+    image: 'TROPISCI-P.jpg',
+    text: 'TROPISCI-P'
   }
 ]
 
